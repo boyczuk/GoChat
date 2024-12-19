8ecc68470e836b6a4c92ceaa58b84033ca3d6acb
adlai <adlaibridsonboyczuk@gmail.com>
2024-12-19 16:09:16 -0500

    Implemented register route + connecting PostgreSQL DB to Go Backend

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,80 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Welcome to the Go server")
-	})
+var db *sql.DB
 
-	fmt.Println("Starting server on http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Error starting server:", err)
+func initDB() {
+	var err error
+	db, err = sql.Open(
+		"postgres",
+		"host=localhost user=postgres password=temppassword dbname=chat_app sslmode=disable",
+	)
+	if err != nil {
+		log.Fatal(err)
 	}
+}
+
+// 'c' contains req http request information
+func registerUser(c *gin.Context) {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	// set req struct equal to info in http request
+	if err := c.BindJSON(&req); err != nil {
+		// Returns JSON formatted response
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": "Invalid input."},
+		)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "Failed to hash password."},
+		)
+		return
+	}
+	_, err = db.Exec(
+		"INSERT INTO users (username, password_hash) VALUES ($1, $2)",
+		req.Username,
+		hashedPassword,
+	)
+	if err != nil {
+		c.JSON(http.StatusConflict,
+			gin.H{"error": "User already exists."},
+		)
+		return
+	}
+
+	// If no errors raised user successfully registered!
+	c.JSON(
+		http.StatusOK,
+		gin.H{"message": "User registered successfully!"},
+	)
+}
+
+func main() {
+	// Connect to DB
+	initDB()
+
+	// Create gin middleware
+	r := gin.Default()
+
+	// Route for registering user
+	r.POST("/register", registerUser)
+	r.Run(":8080")
 }
