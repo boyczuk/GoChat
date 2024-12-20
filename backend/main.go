d2aef04842ce45f27d68629d639d5ff1b52c5570
adlai <adlaibridsonboyczuk@gmail.com>
2024-12-19 21:26:28 -0500

    loginUser function + sessionToken creation

--- backend/main.go
+++ backend/main.go
@@ -4,8 +4,10 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -21,6 +23,76 @@ func initDB() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func loginUser(c *gin.Context) {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": "Invalid input."},
+		)
+		return
+	}
+
+	var dbPasswordHash string
+	var userID int
+
+	err := db.QueryRow(
+		"SELECT id, password_hash FROM users WHERE username = $1", req.Username,
+	).Scan(&userID, &dbPasswordHash)
+
+	if err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "Invalid username or password."},
+		)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(dbPasswordHash), []byte(req.Password))
+	if err != nil {
+		c.JSON(
+			http.StatusUnauthorized,
+			gin.H{"error": "Invalid username or password."},
+		)
+		return
+	}
+
+	// Random string that acts as session token
+	sessionToken := uuid.New().String()
+
+	// 24 hours from now it expires
+	expiresAt := time.Now().Add(24 * time.Hour)
+
+	_, err = db.Exec(
+		"INSERT INTO sessions (user_id, session_token, expires_at) VALUES ($1, $2, $3)",
+		userID, sessionToken, expiresAt,
+	)
+
+	if err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "Failed to create session for user."},
+		)
+		return
+	}
+
+	c.SetCookie(
+		"session_token",
+		sessionToken,
+		3600*24,
+		"/",
+		"localhost",
+		false,
+		true,
+	)
+
+	c.JSON(http.StatusOK, gin.H{"message": "Login successful."})
 }
 
 // 'c' contains req http request information
@@ -76,5 +148,6 @@ func main() {
 
 	// Route for registering user
 	r.POST("/register", registerUser)
+	r.POST("/login", loginUser)
 	r.Run(":8080")
 }
